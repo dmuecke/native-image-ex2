36ce84253fa3457c11bd5a1594b6100614763b09
muecked <dieter.muecke@lidl.com>
2021-05-21 12:24:32 +0200

    pom.xml update

--- src/main/kotlin/com/sit/adbs/nativeimageex2/NativeImageEx2Application.kt
+++ src/main/kotlin/com/sit/adbs/nativeimageex2/NativeImageEx2Application.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
+
 @SpringBootApplication
 class NativeImageEx2Application
 
--- src/main/kotlin/com/sit/adbs/nativeimageex2/config/RouteFilterConfig.kt
+++ src/main/kotlin/com/sit/adbs/nativeimageex2/config/RouteFilterConfig.kt
@@ -1,5 +1,6 @@
 package com.sit.adbs.nativeimageex2.config
 
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class RouteFilterConfig {
 
-
+    @RefreshScope
     @Bean("gatewayRouteLocator")
     fun routeLocator(routeBuilder: RouteLocatorBuilder): RouteLocator {
         return routeBuilder.routes().build()
